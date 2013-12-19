bc81dfd1442d3ca0b190095f0e5d5ab96b46dfc4
Patrick Marabeas <patrick@pulse-dev.com.au>
2013-12-19 23:42:58 +1100

    Initial code base

---
+++ demo.js
@@ -0,0 +1,80 @@
+
+
+var module = angular.module( 'demo', ['ngDiscreteUI'] );
+
+
+module.directive( 'mydirwhichtriggersadoption', [ 'AdoptionAgency', function(AdoptionAgency) {
+	return {
+
+		require: 'discrete',
+		link: function( scope, element, attrs) {
+
+			var elementHeight = document.getElementById(attrs.id).offsetTop;
+			var oldElm = document.getElementById(attrs.id);
+
+			angular.element( window ).bind( 'scroll', function() {
+
+				if( scope.discrete ) {
+					var newElm = document.getElementById(attrs.id + '_discrete');
+
+					newElm.style.width = oldElm.offsetWidth + 'px';
+					newElm.style.left = oldElm.offsetLeft + 'px';
+
+				}
+
+				var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+				if( scrollTop > elementHeight ) {
+
+					scope.$apply( function () {
+						scope.discrete = true;
+					});
+				}
+
+				else {
+					scope.$apply( function () {
+						scope.discrete = false;
+					});
+				}
+
+
+			});
+
+			angular.element( window ).bind( 'resize', function() {
+
+
+				if( scope.discrete ) {
+					var newElm = document.getElementById(attrs.id + '_discrete');
+
+					newElm.style.width = oldElm.offsetWidth + 'px';
+					newElm.style.left = oldElm.offsetLeft + 'px';
+
+				}
+
+
+			});
+
+
+
+
+		}
+
+	}
+}]);
+
+
+
+
+module.directive( 'test', [ 'AdoptionAgency', function(AdoptionAgency) {
+	return {
+
+		link: function( scope, element, attrs) {
+
+			angular.element( element ).bind( 'click', function() {
+				element[0].style.backgroundColor = 'blue';
+			});
+
+		}
+	}
+
+}]);
---
+++ ng-DiscreteUI.js
@@ -0,0 +1,71 @@
+/* ng-DiscreteUI.js v1.0.0
+ * https://github.com/patrickmarabeas/ng-DiscreteUI.js
+ *
+ * Copyright 2013, Patrick Marabeas http://pulse-dev.com
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * Date: 19/12/2013
+ */
+
+var module = angular.module( 'ngDiscreteUI', [] );
+
+module.factory( 'AdoptionAgency', function() {
+	return {
+		adopts: [],
+		addAdopts: function( id, values ) {
+			this.adopts[id] = values;
+		},
+		custody: function( newElm, oldElm ) {
+			while (oldElm.hasChildNodes()) {
+				newElm.appendChild(oldElm.removeChild(oldElm.firstChild))
+			}
+		},
+		create: function( id ) {
+			var element = this.adopts[id];
+			var oldElm = document.getElementById( id );
+			var newElm = document.createElement( element.tag );
+			newElm.setAttribute( 'id', element.newElm );
+			element.container.appendChild( newElm );
+			this.custody( newElm, oldElm );
+		},
+		destroy: function( id ) {
+			var element = this.adopts[id];
+			var oldElm = document.getElementById( id );
+			var newElm = document.getElementById( element.newElm );
+			if ( newElm ) {
+				this.custody( oldElm, newElm );
+				newElm.parentNode.removeChild( newElm );
+			}
+		}
+	}
+});
+
+
+module.directive( 'discrete', [ 'AdoptionAgency', function( AdoptionAgency ) {
+	return {
+
+		controller: function( $scope, $element, $attrs ) {
+			$scope.condition = false;
+		},
+		scope: true,
+		link: function( scope, element, attrs ) {
+
+			AdoptionAgency.addAdopts( attrs.id, {
+				newElm: attrs.id + '_discrete',
+				tag: element[0].nodeName,
+				container: document.getElementById( attrs.id ).parentNode
+			});
+
+			scope.$watch('discrete', function( adopt ){
+				if( adopt ){
+					AdoptionAgency.create( attrs.id );
+				}
+				else{
+					AdoptionAgency.destroy( attrs.id );
+				}
+			});
+
+		}
+	}
+}]);
